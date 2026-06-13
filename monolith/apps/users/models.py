0b40697151f22c45d193fc93d405b1c2f187e754
apetrova <apetrova@lean-it.ru>
2026-06-13 20:00:24 +0200

    app settings and docker

---
+++ monolith/apps/users/models.py
@@ -0,0 +1,3 @@
+from .internal.user import User
+
+__all__ = ["User"]
---
+++ monolith/resources/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.shortcuts import render
+from django.urls import include, path
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+urlpatterns = [
+    path("", home, name="home"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
