6f753c23a823203fb100ee6e3535a809c9c9376e
apetrova <apetrova@lean-it.ru>
2026-06-21 01:44:48 +0100

    add methods to users client

--- distributed/shared/shared/clients/user_service_client.py
+++ distributed/shared/shared/clients/user_service_client.py
@@ -6,15 +6,7 @@ class UserServiceClient:
 
     BASE_URL = "http://users-service:8000"
 
-    @classmethod
-    def get_user(cls, user_id: int):
-        response = requests.get(
-            f"{cls.BASE_URL}/api/accounts/users/{user_id}"
-        )
-
-        response.raise_for_status()
-        data = response.json()
-
+    def json_to_user_dto(data):
         return UserDTO(
             id=data["id"],
             username=data["username"],
@@ -25,3 +17,36 @@ class UserServiceClient:
             phone=data["phone"],
             bio=data["bio"]
         )
+
+    @classmethod
+    def get_user(cls, user_id: int):
+        response = requests.get(
+            f"{cls.BASE_URL}/api/accounts/users/{user_id}"
+        )
+
+        response.raise_for_status()
+        data = response.json()
+
+        return cls.json_to_user_dto(data)
+    
+    @classmethod
+    def get_all_players(cls):
+        response = requests.get(
+            f"{cls.BASE_URL}/api/accounts/players"
+        )
+
+        response.raise_for_status()
+        data = response.json()
+
+        return [cls.json_to_user_dto(player) for player in data]
+    
+    @classmethod
+    def get_all_referees(cls):
+        response = requests.get(
+            f"{cls.BASE_URL}/api/accounts/referees"
+        )
+
+        response.raise_for_status()
+        data = response.json()
+
+        return [cls.json_to_user_dto(referee) for referee in data]
